48040bf698bdef2affbbd1a5bce2f54ce582c498
TatuPutto <tatuputto@gmail.com>
2017-04-12 11:35:04 +0300

    Server scripts and css correctly

--- scripts.js
+++ scripts.js
@@ -1,19 +1,28 @@
-(function setImageAspectRatio() {
+$(document).ready(function setImageAspectRatio() {
     var thumbnails = $('img');
-    var height = thumbnails[0].offsetWidth / 4 * 3;
+    var height = thumbnails[1].offsetWidth / 4 * 3;
     //console.log(document.getElementsByClassName('thumbnail-container')[0].offsetWidth);
-    for(var i = 0; i < thumbnails.length; i++) {
+    for(var i = 1; i < thumbnails.length; i++) {
         thumbnails[i].style.height = height + 'px';
     }
-})();
+});
 
-function showLargePicture() {
-    $(document.body).append('<div class="image-carousel"><img class="project-image-large" src="./images/gist-management-app-front.png"/></div>');
+function showLargePicture(imageSrc) {
+    //$(document.body).append('<div class="overlay"><div class="image-carousel" onclick="stop(stopPropagation)"><img class="project-image-large" src="./images/gist-management-app-front.png"/></div></div>');
+    $('.overlay').css('display', 'inline');
+    $('.image-carousel img').attr('src', imageSrc);
+    $('.image-carousel').animate({opacity: 1}, 300);
 
     setTimeout(function () {
-        $(document.body).on('click', function () {
-            $('.image-carousel').remove();
-            $(document.body).off('click');
+        $('.overlay').on('click', function () {
+            $('.overlay').off('click');
+            $('.image-carousel').animate({opacity: 0}, 300, function () {
+                $('.overlay').css('display', 'none');
+            });
         });
     }, 100);
+}
+
+function stopPropagation(event) {
+    event.stopPropagation();
 }
